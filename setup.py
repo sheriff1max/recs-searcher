0478b8a70f93eed06ee319a244620ae6c3c64fc7
morph1max <kobelevmaxim48@gmail.com>
2023-12-19 23:27:49 +0700

    update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from setuptools import setup, find_packages
+import os
 
 
 def readme():
@@ -7,7 +8,7 @@ def readme():
 
 setup(
   name='recs-searcher',
-  version='0.0.1',
+  version='0.0.2',
   author='sheriff1max',
   author_email='kobelevmaxim48@gmail.com',
   description='Search engine and registry error corrector',
@@ -15,7 +16,28 @@ setup(
   long_description_content_type='text/markdown',
   url='https://github.com/sheriff1max/recs-searcher',
   packages=find_packages(),
-  install_requires=['requests>=2.25.1'],
+  install_requires=[
+    'fastapi==0.104.1',
+    'chroma-hnswlib==0.7.3',
+    'chromadb',
+    'datasets==2.14.5',
+    'faiss-cpu',
+    'gensim',
+    'sentence-transformers==2.2.2',
+    'natasha==0.10.0',
+    'numpy==1.26.0',
+    'pandas==2.1.1',
+    'pymorphy2==0.8',
+    'pymorphy3==1.2.1',
+    'scikit-learn==1.3.1',
+    'scipy==1.11.3',
+    'spacy==3.7.2',
+    'thefuzz==0.20.0',
+    'torch==2.1.0',
+    'torchvision==0.16.0',
+    'words2numsrus==0.1.0',
+    'wordtodigits==1.0.2',
+  ],
   classifiers=[
     'Programming Language :: Python :: 3',
   ],
